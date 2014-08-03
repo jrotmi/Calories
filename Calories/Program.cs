ca2f4725e30320a2043345f6679498225b2b61f1
jrotmi <jeffotmi@hotmail.com>
2014-08-03 12:34:47 +0200

    MyFirstProject
    For Nino

---
+++ Calories/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many Calories did you eat today?");
+            string userCalories = Console.ReadLine();
+
+            string message = "";
+
+            int inCalories;
+            bool isValid = int.TryParse(userCalories, out inCalories);
+            if (isValid)
+            {                   
+                if (inCalories <= 1500)
+                    message = "You didn't eat too many calories today!";
+                else if ((inCalories > 1500) && (inCalories <= 2000))
+                    message = "You ate a moderate amount of calories today!";
+                else if (inCalories > 2000)
+                    message = "You ate A LOT!";
+            }
+            else
+                message = "Please insert a string of numbers";
+
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
